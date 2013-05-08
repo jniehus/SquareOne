d7990088c78e536658bc396d16376a91964497cb
Joshua Niehus <jm.niehus@gmail.com>
2013-05-07 21:08:13 -0700

    got rid of ref qualifiers, arrays in D are fat pointers and don't need refing

--- particle.d
+++ particle.d
@@ -3,14 +3,14 @@
  */
 
 module particle;
- 
+
 import std.stdio, std.math, std.conv, std.traits;
 import vector3;
 
 /*
  * Simple particle (point mass)
- */ 
-class Particle(T) 
+ */
+class Particle(T)
   if (isFloatingPoint!T)
 {
     alias Vector3!T Vector;
@@ -26,14 +26,14 @@ class Particle(T)
         m_inverseMass       = 1;
         m_forceAccumulation = m_acceleration; // starts out with gravity...
     }
-    
-    this (ref Vector pos, ref Vector vel, T invMass = 1, T dmp = 0.995) {
+
+    this (Vector pos, Vector vel, T invMass = 1, T dmp = 0.995) {
         m_position          = pos;
         m_velocity          = vel;
         m_acceleration      = Vector(0, -10, 0); // -10 m/s^2 gravity
         m_damping           = dmp;
         m_inverseMass       = invMass;
-        m_forceAccumulation = m_acceleration;        
+        m_forceAccumulation = m_acceleration;
     }
 
     /*--------------------------------------------
@@ -41,20 +41,20 @@ class Particle(T)
      -------------------------------------------*/
     private Vector m_position;
     public {
-      @property Vector position()                 { return m_position; }
-      @property Vector position(ref Vector value) { return m_position = value; }
+      @property Vector position()             { return m_position; }
+      @property Vector position(Vector value) { return m_position = value; }
     }
-    
+
     private Vector m_velocity;
     public {
-      @property public Vector velocity()                 { return m_velocity; }
-      @property public Vector velocity(ref Vector value) { return m_velocity = value; }        
+      @property public Vector velocity()             { return m_velocity; }
+      @property public Vector velocity(Vector value) { return m_velocity = value; }
     }
-    
+
     private Vector m_acceleration;
     public {
-      @property public Vector acceleration()                 { return m_acceleration; }
-      @property public Vector acceleration(ref Vector value) { return m_acceleration = value; }        
+      @property public Vector acceleration()             { return m_acceleration; }
+      @property public Vector acceleration(Vector value) { return m_acceleration = value; }
     }
 
     /* Holds the ammount of damping applied to linear motion.
@@ -66,66 +66,73 @@ class Particle(T)
       @property public T damping()        { return m_damping; }
       @property public T damping(T value) { return m_damping = value; }
     }
-     
+
     /* store the inverse mass since it makes representing mass extremes easier:
      * if infinite invMass = 0, zero mass = very big...
      * Accessor fucntions to control this property */
     private T m_inverseMass;
     public {
-      @property public T inverseMass()        { return m_inverseMass; }   
-      @property public T inverseMass(T value) { return m_inverseMass = value; } 
-      
-      @property T mass() {
+      @property public T inverseMass()        { return m_inverseMass; }
+      @property public T inverseMass(T value) { return m_inverseMass = value; }
+
+      @property T mass()
+      {
         if (m_inverseMass == 0) {
           return T.max;
         }
         else {
           return (1/m_inverseMass);
         }
-      }        
-     
-      @property T mass(T value) {
+      }
+
+      @property T mass(T value)
+      {
         assert(value != 0);
         return m_inverseMass = 1/value;
-      }   
-      
+      }
+
       bool hasFiniteMass() { return (m_inverseMass >= 0.0f); }
     }
-    
+
     private Vector m_forceAccumulation;
-        
+
     /*---------------------------------------------
      * METHODS
      *-------------------------------------------*/
     public {
-      void integrate(T timeStep) {  
+      void integrate(T timeStep)
+      {
         assert(timeStep > 0);
-        
+
         // update linear position
         m_position.addScaledVector(m_velocity, timeStep);
-        
+
         // calculate acceleration from force
         Vector resultingAcceleration = m_acceleration;
         resultingAcceleration.addScaledVector(m_forceAccumulation, m_inverseMass);
-        
+
         // update linear velocity
         m_velocity.addScaledVector(resultingAcceleration, timeStep);
-        
+
         // impose drag
         m_velocity *= std.math.pow(damping, timeStep);
       }
-     
-      void clearAccumulator() {
+
+      void clearAccumulator()
+      {
         m_forceAccumulation.clear();
       }
-    
-      void addForce(Vector force) {
+
+      void addForce(Vector force)
+      {
         m_forceAccumulation += force;
-      } 
-    }    
-    
+      }
+    }
+
     /* Object method overrides */
-    string toString() {
+    override
+    string toString()
+    {
         string particleStr = "Particle!" ~ T.stringof ~ "\n";
         particleStr ~= "  position: "       ~ to!string(m_position) ~ "\n";
         particleStr ~= "  velocity: "       ~ to!string(m_velocity) ~ "\n";
@@ -140,7 +147,7 @@ unittest
     auto p1         = new Particle!double();
     auto nullVector = Vector3!double([0, 0, 0]);
     auto gravity    = Vector3!double([0, -10, 0]);
-    
+
     assert(p1.position            == nullVector);
     assert(p1.velocity            == nullVector);
     assert(p1.acceleration        == gravity);
@@ -148,21 +155,21 @@ unittest
     assert(p1.inverseMass         == 1);
     assert(p1.mass                == 1);
     assert(p1.m_forceAccumulation == gravity);
-    
+
     auto pos = Vector3!real(12.14, -134.79, 200);
     auto vel = Vector3!real(3000, -1.99, 0);
 
-    alias Particle!real RParticle; 
+    alias Particle!real RParticle;
     auto p2 = new RParticle(pos, vel);
-    
+
     assert(p2.position            == Vector3!real([12.14, -134.79, 200]));
     assert(p2.velocity            == Vector3!real([3000, -1.99, 0]));
     assert(p2.acceleration        == Vector3!real([0, -10, 0]));
     assert(p2.damping             == 0.995f);
     assert(p2.inverseMass         == 1);
     assert(p2.mass                == 1);
-    assert(p2.m_forceAccumulation == Vector3!real([0, -10, 0])); 
-     
+    assert(p2.m_forceAccumulation == Vector3!real([0, -10, 0]));
+
     writeln("Particle constructor unittest passed. \n", p1);
 }
 
@@ -174,13 +181,11 @@ unittest
     auto wind = Vector3!float([100, 3.4447, 0]);
     p1.addForce(wind);
     p1.integrate(0.3);
-    
+
     assert(p1.position            == Vector3!float([100, 9.24813, 300]));
     assert(p1.velocity            == Vector3!float([299.386, -6.26501, 0]));
     assert(p1.acceleration        == Vector3!float([0, -10, 0]));
     assert(p1.m_forceAccumulation == Vector3!float([100, -6.5553, 0]));
-    
+
     writeln("Particle methods unittest passed. \n", p1);
 }
-
-void main() {}
--- vector3.d
+++ vector3.d
@@ -3,62 +3,67 @@
  */
 
 module vector3;
- 
+
 import std.stdio, std.math, std.conv, std.traits;
 
 struct Vector3(T) if (isFloatingPoint!T)
 {
     /// vector data: x = 0, y = 1, z = 2... as one would hopefully expect...
     T[3] v = [0, 0, 0];
-    
-    this (T[] v_in) {
+
+    this (T[] v_in)
+    {
         v[] = v_in;
     }
 
-    this (T x, T y, T z) {
+    this (T x, T y, T z)
+    {
         v[0] = x;
         v[1] = y;
         v[2] = z;
     }
-    
+
     /**
-     * METHODS 
+     * METHODS
      */
     void invert() { v[] *= -1; }
-    
+
     /// magnitude: Pythagoras theorm for 3D
     @property T magnitude() { return std.math.sqrt(squareMagnitude); }
-    
-    T squareMagnitude() {
+
+    T squareMagnitude()
+    {
         T m2 = 0;
         foreach(i; v) {
             m2 += i*i;
         }
-        
+
         return m2;
     }
-    
-    void normalize() {
+
+    void normalize()
+    {
         T n = magnitude();
         if (n > 0) {
             v[] *= 1/n;
         }
     }
-    
+
     /**
      * add scaled vector
      * Params:
      *  b     = Vector3 to be scaled and added
      *  scale = scale factor for b
      */
-    void addScaledVector(ref Vector3 b, T scale) {
+    void addScaledVector(Vector3 b, T scale)
+    {
         v[] += (b * scale).v[];
     }
-    
+
     void clear() {
         v = [0, 0, 0];
     }
-    
+
     /**
      * component product
      * Params:
@@ -66,51 +71,53 @@ struct Vector3(T) if (isFloatingPoint!T)
      * Returns:
      *  Vector3 who's components are multiple of this vector and b's
      */
-    Vector3 componentProduct(ref Vector3 b) {
+    Vector3 componentProduct(Vector3 b)
+    {
         auto tmp = v[].dup;
         tmp[] *= b.v[];
         return Vector3(tmp);
     }
-    
-    void componentProductUpdate(ref Vector3 b) {
+
+    void componentProductUpdate(Vector3 b)
+    {
         v[] *= b.v[];
     }
-    
-    Vector3 crossProduct(ref const Vector3 b) {
+
+    Vector3 crossProduct(Vector3 b)
+    {
         return Vector3((v[1] * b.v[2]) - (v[2] * b.v[1]),
                        (v[2] * b.v[0]) - (v[0] * b.v[2]),
                        (v[0] * b.v[1]) - (v[1] * b.v[0]));
     }
-    
+
     /**
      * OPERATORS
      */
-    Vector3 opBinary(string op)(ref Vector3 rhs) if (op != "*") {
+    Vector3 opBinary(string op)(Vector3 rhs) if (op != "*")
+    {
         auto tmp = v[].dup;
         static if (op == "%") {
             return Vector3((v[1] * rhs.v[2]) - (v[2] * rhs.v[1]),
                            (v[2] * rhs.v[0]) - (v[0] * rhs.v[2]),
                            (v[0] * rhs.v[1]) - (v[1] * rhs.v[0]));
-        } 
-        else static if (op == "+") {
-            tmp[] += rhs.v[];
+        }
+        else {
+            mixin("tmp[] " ~ op ~ "= rhs.v[];");
             return Vector3(tmp);
-        } 
-        else static if (op == "-") {
-            tmp[] -= rhs.v[];
-            return Vector3(tmp);
-        }      
+        }
     }
 
     /// scalar multiplication
-    Vector3 opBinary(string op)(T rhs) if (op == "*") {
+    Vector3 opBinary(string op)(T rhs) if (op == "*")
+    {
         auto tmp = v[].dup;
         tmp[] *= rhs;
-        return (Vector3(tmp));        
-    }    
-    
+        return (Vector3(tmp));
+    }
+
     /// dot product
-    T opBinary(string op)(ref Vector3 rhs) if (op == "*") {
+    T opBinary(string op)(Vector3 rhs) if (op == "*")
+    {
         auto tmp = v[].dup;
         T value = 0;
         for (int i = 0; i < 3; i++) {
@@ -118,33 +125,35 @@ struct Vector3(T) if (isFloatingPoint!T)
         }
         return value;
     }
-    
+
     /// cross | dot | add | subtract on self
-    Vector3 opOpAssign(string op)(ref Vector3 rhs) {
+    Vector3 opOpAssign(string op)(Vector3 rhs)
+    {
         v = opBinary!op(rhs).v;
         return this;
     }
-    
+
     /// scalar multiply on self
-    Vector3 opOpAssign(string op)(T rhs) {
+    Vector3 opOpAssign(string op)(T rhs)
+    {
         v = opBinary!op(rhs).v;
         return this;
-    }    
-    
+    }
+
     /// index operator
     ref T opIndex(uint i) { return v[i]; }
-    
+
     /// overide opEquals
-    bool opEquals(ref Vector3 rhs) {
+    bool opEquals(Vector3 rhs)
+    {
         for (int i = 0; i < 3; ++i) {
             if (!approxEqual(v[i], rhs.v[i], 1e-4, 1e-5)) {
                 return false;
             }
         }
-        
         return true;
-    }    
-    
+    }
+
     /**
      * Object overides
      */
@@ -164,7 +173,7 @@ unittest
     assert(v0.toString() == "double[0, 0, 0]");
     assert(v1.toString() == "float[7, 3, 9]");
     assert(v2.v == [7, 12, 11]);
-    
+
     assert(is( typeof(v0) == Vector3!double));
     writeln("Vector3 constructor unittest passed\n", q);
 }
@@ -172,11 +181,11 @@ unittest
 // unittest methods
 unittest
 {
-    auto v = Vector3!real(4, 7, 8);   
+    auto v = Vector3!real(4, 7, 8);
     v.invert();
     assert(v.toString() == "real[-4, -7, -8]");
-    v.invert();   
-    assert(v.magnitude >= 11.3578f && v.magnitude <= 11.3579f);   
+    v.invert();
+    assert(v.magnitude >= 11.3578f && v.magnitude <= 11.3579f);
     v.normalize();
     assert(v.toString() == "real[0.35218, 0.616316, 0.704361]");
     assert((v.crossProduct(v)).v == [0, 0, 0]);
@@ -190,20 +199,20 @@ unittest
     v2.addScaledVector(v3, 3);
     assert(v2.v == [14, 31, 26]);
     assert(v3.v == [4, 10, 8]);
-    
+
     auto v4 = v3.componentProduct(v2);
     assert(v4.v == [56, 310, 208]);
     assert(v2.v == [14, 31, 26]);
     assert(v3.v == [4, 10, 8]);
-    
+
     v3.componentProductUpdate(v4);
     assert(v3.v == [224, 3100, 1664]);
     assert(v4.v == [56, 310, 208]);
-    
+
     v4.clear();
     assert(v4.v == [0, 0, 0]);
-    
-    writeln("Vector3 method unittest passed\n", v4); 
+
+    writeln("Vector3 method unittest passed\n", v4);
 }
 
 // unittest operator overloads
@@ -212,57 +221,57 @@ unittest
     auto v = Vector3!real(2, 4, 6);
     auto q = Vector3!real(5, 7, 9);
     auto s = Vector3!real(5, 7, 9);
-    
+
     auto boolChk = (s == q);
     auto boolChk2 = (s != v);
     assert(boolChk);
     assert(boolChk2);
-    
+
     // --- addition subtractions
     q -= v;
     v += q;
     assert(q.v == [3, 3, 3]);
     assert(v.v == [5, 7, 9]);
-    
+
     auto a = q - v;
     assert(a.v == [-2, -4, -6]);
     assert(q.v == [3, 3, 3]);
-    assert(v.v == [5, 7, 9]);    
-    
+    assert(v.v == [5, 7, 9]);
+
     auto d = v + a;
     assert(d.v == [3, 3, 3]);
     assert(a.v == [-2, -4, -6]);
     assert(q.v == [3, 3, 3]);
-    assert(v.v == [5, 7, 9]);    
-    
+    assert(v.v == [5, 7, 9]);
+
     // --- vector product / cross product
     auto r = v % q;
     assert(r.v == [-6, 12, -6]);
     assert(q.v == [3, 3, 3]);
     assert(v.v == [5, 7, 9]);
-    
+
     r %= v;
-    assert(r.v == [150, 24, -102]);   
+    assert(r.v == [150, 24, -102]);
     assert(r[1] == 24);
     r[1] += 1;
     assert(r.v == [150, 25, -102]);
     r[1] -= 1;
-    assert(r.v == [150, 24, -102]);       
+    assert(r.v == [150, 24, -102]);
     assert(v.v == [5, 7, 9]);
 
     // --- Multiplications
     auto x = r * q;
     assert(x == 216);
-    assert(r.v == [150, 24, -102]);   
-    assert(q.v == [3, 3, 3]);    
-    
+    assert(r.v == [150, 24, -102]);
+    assert(q.v == [3, 3, 3]);
+
     auto n = Vector3!double(2, 2, 2);
     auto m = n * 4.0;
     assert(m.v == [8, 8, 8]);
     assert(n.v == [2, 2, 2]);
-    
+
     m *= 2;
     assert(m.v == [16, 16, 16]);
-    
+
     writeln("Vector3 operator overload unittest passed\n", m);
 }
